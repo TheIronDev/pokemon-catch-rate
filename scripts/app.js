d5fc357fdd8d04dfee5dc2f9db45fc7f32f9ea6c
Tyler Stark <theirondeveloper@gmail.com>
2014-12-17 00:57:24 -0800

    Added Pokeballs, and some dropdowns

--- scripts/app.js
+++ scripts/app.js
@@ -7,15 +7,36 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-
+    return {
+      pokemon: this.pokemon(),
+      pokeballs: this.pokeballs()
+    };
+  },
+  pokemon: function() {
     return this.store.findAll('pokemon');
+  },
+  pokeballs: function() {
+    return this.store.findAll('pokeball');
   }
+});
+
+App.IndexController = Ember.Controller.extend({
+  selectedPokemon: 2,
+  selectedLevel: 1,
+  levels: [1,2,3,4,5]
 });
 
 App.Pokemon = DS.Model.extend({
   name: DS.attr('string'),
   maxHP: DS.attr('string'),
-  currentHP: DS.attr('string')
+  currentHP: DS.attr('string'),
+  pokeball: DS.hasMany('pokeball', {async: true})
+});
+
+App.Pokeball = DS.Model.extend({
+  name: DS.attr('string'),
+  rate: DS.attr('number'),
+  pokemon: DS.belongsTo('pokemon', {async: true})
 });
 
 App.Pokemon.reopenClass({
@@ -31,6 +52,32 @@ App.Pokemon.reopenClass({
       name: 'Ivysaur',
       currentHP: 90,
       maxHP: 100
+    },
+    {
+      id: 3,
+      name: 'Venasaur',
+      currentHP: 1,
+      maxHP: 100
+    }
+  ]
+});
+
+App.Pokeball.reopenClass({
+  FIXTURES : [
+    {
+      id: 1,
+      name: 'PokeBall',
+      rate: 1
+    },
+    {
+      id: 2,
+      name: 'Great Ball',
+      rate: 1.5
+    },
+    {
+      id: 3,
+      name: 'Ultra Ball',
+      rate: 2
     }
   ]
 });
